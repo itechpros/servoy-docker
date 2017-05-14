b6f36624961ae3c95ceef98da5a6b3e9850441c9
goldcougar <scott@itechpros.com>
2017-05-14 17:53:46 -0400

    Delete servoy_docker.sh

--- demo/servoy_generic_dynamic_db/servoy_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Running Servoy Start Script"
-echo "Filling templates with environmental variables"
-ep servoy_server_docker.sh
-ep servoy.properties
-
-echo "Starting Servoy"
-./servoy_server_docker.sh
